760082f6fa0a8efe3b18867188e752a0e445d4d7
pankajtransact <82398081+pankajtransact@users.noreply.github.com>
2022-09-06 00:14:59 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core'
 import axios from 'axios'
+import * as fs from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const swaggerUrl: string = core.getInput('swaggerUrl')
+    const swaggerUrl: string = core.getInput('swaggerPath')
+    const swaggerType: string = core.getInput('swaggerType')
+    const swaggerPathType: string = core.getInput('swaggerPathType')
     const apiManagementEndpointUrl: string = core.getInput(
       'apiManagementEndpointUrl'
     )
@@ -18,6 +21,14 @@ async function run(): Promise<void> {
     if (creds == null) {
       core.setFailed('Missing access token from input')
     }
+    if (swaggerType = null){
+      core.setFailed('Missing swagger type. Expecting json or yaml')
+    }
+    
+   if (swaggerPathType = null){
+      core.setFailed('Missing swagger path type. Expecting url or local')
+    }
+    
     //Request an OAUth Token
     core.info('Parse Json')
     const jsonObj = JSON.parse(creds)
@@ -38,9 +49,25 @@ async function run(): Promise<void> {
     } catch (err) {
       core.error(err)
     }
+    
+    var formatType = '';
+    var linkType = '';
+    
+    if (swaggerPathType == 'url'){
+      linkType = '-link';
+    }
+    
+    if (swaggerType == 'json'){
+       formatType = '+json' + linkType;
+    }else{
+       formatType = linkType
+    }
+    
+    
+     
     const putData = {
       properties: {
-        format: 'openapi+json-link',
+        format: 'openapi'+ formatType,
         value: `${swaggerUrl}`,
         path: 'hallsoll'
       }
